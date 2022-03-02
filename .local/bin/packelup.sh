10be769196e24f57bd063c6eba7cb27c40f5e003
lordagustin22 <niconecrofilo589@gmail.com>
2022-03-02 18:15:59 -0300

    Change path

--- .local/bin/packelup.sh
+++ .local/bin/packelup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-dpath=~/Descargas/duan/backups
+dpath=~/Documentos/Bash
 list=$dpath/program-list
-if ! [[ -e $list ]]
+if [[ ! -e $list ]]
 then
 touch $list
 fi
 echo 'Programs installed on Artix Linux as of today (includes AUR)' > $list && date | awk '{print $1,$2,$3,$4}' >> $list && pacman -Qeq >> $list
-notify-send --urgency=low "Lista de programas actualizada"
+notify-send --urgency=normal "Lista de programas actualizada"
