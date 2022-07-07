53e7dd06d426c722730eec16f65ab20ebbcfca3a
lordagustin22 <niconecrofilo589@gmail.com>
2022-07-07 19:27:45 -0300

    Complete polybar overhaul. Now looks better

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,7 +5,8 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar cliffmain 2>&1 | tee -a /tmp/polybar.log & disown
-
-echo "Polybar ativado malongo nwn"
