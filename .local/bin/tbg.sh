b225f4c9e56d68888d98449fafdcb04115b65415
lordagustin22 <niconecrofilo589@gmail.com>
2023-01-18 10:56:54 -0300

    Tmux script for opening sessions

--- .local/bin/tbg.sh
+++ .local/bin/tbg.sh
@@ -5,20 +5,22 @@
 session_name='bg'
 
 # Create the tmux session if it doesn't exist
-if [ "$(tmux list-sessions | grep $session_name)" = "" ]; then
+if [ "$(tmux list-sessions | grep $session_name)" != 'bg' ]; then
   tmux new-session -s $session_name
 fi
 
-# Attach to the session
-tmux attach-session -t $session_name
-
 # Create the windows
-tmux new-window -n 'tremc'
-tmux new-window -n 'cointop'
-tmux new-window -n 'htop'
+tmux new-window -t $session_name -n 'tremc'
+tmux new-window -t $session_name -n 'cointop'
+tmux new-window -t $session_name -n 'htop'
 
 # Send the keys to the different windows
 tmux send-keys -t $session_name:tremc "tremc" C-m
 tmux send-keys -t $session_name:c "cointop" C-m
 tmux send-keys -t $session_name:h "htop" C-m
-tmux kill-window -tbg:0
+tmux kill-window -t $session_name:0
+
+# Not used
+# tmux kill-window -t $session_name:4
+# tmux kill-window -t $session_name:5
+# tmux kill-window -t $session_name:6
