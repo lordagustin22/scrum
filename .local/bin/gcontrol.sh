bb191643f415b7eec31f131c5a5caec9b0eb5350
lordagustin22 <niconecrofilo589@gmail.com>
2023-01-29 22:56:47 -0300

    Better written and safer control of commits

--- .local/bin/gcontrol.sh
+++ .local/bin/gcontrol.sh
@@ -5,15 +5,15 @@ file=$HOME/Documentos/tokengit.txt
 # adds everything and makes a commit
 adding() {
     git add .
-    read -p "Enter a commit message: " commit_message
-    read -p "Enter the file you want to commit: " commit_file
+    read -p "Mensaje para el commit: " commit_message
+    read -p "Archivo para el commit: " commit_file
     git commit -m "$commit_message" $commit_file
 }
 
 pushing() {
     cat $file | xargs -I '{}' echo -n {} | xclip -sel clip
     git push
-    echo "" | xclip -sel clip #emtpies xclip for safety
+    echo "" | xclip -sel clip # empties xclip for safety
 }
 
 ask() {
