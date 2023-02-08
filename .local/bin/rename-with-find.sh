5daa38d342456994f0ef2555ab48d96817b367ca
lordagustin22 <niconecrofilo589@gmail.com>
2023-02-08 12:15:45 -0300

    Converts flac to mp3 including subdirs

--- .local/bin/rename-with-find.sh
+++ .local/bin/rename-with-find.sh
@@ -5,10 +5,10 @@
 # find -maxdepth 2 -name '*.flac.mp3' | xargs -I{} basename {} | sed -s s/.flac//
 # find -maxdepth 2 -name '*.flac' -exec rm {} +
 
-# El que encontré en internet
 echo "Desea convertir todos los directorios y subdirectorios a mp3?"
 read -p "'y' para sí, 'n' para no: " rta
 if [[ $rta = 'y' ]]; then
+# El que encontré en internet
     find . -type f -name '*.flac' -print -exec sh -c 'i="{}"; ffmpeg -i "$i" -y "${i%.flac}".mp3 && rm -f "$i"' \;
     notify-send "Conversion de flac a mp3 completada nwn"
 elif [[ $rta = 'n' ]]; then
