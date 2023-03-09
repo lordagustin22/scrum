568333bfcfcc60395eb6ab993daa71a28dc24e0b
lordagustin22 <niconecrofilo589@gmail.com>
2023-03-09 20:48:27 -0300

    Transfer folders with rsync automatically

--- .local/bin/rtransfer.sh
+++ .local/bin/rtransfer.sh
@@ -3,7 +3,12 @@
 # this is a work in progress
 readonly BACKUP_DIRS=($HOME/Documentos/duanland.neocities.org $HOME/Documentos/UTN $HOME/Documentos/Cosas-japo $HOME/Descargas/PruebaTraduccion-IVREA $HOME/Imagenes $XDG_DATA_HOME/vimwiki)
 readonly RSYNC_DEFAULTS="-Paz"
-readonly DESTINO=/run/media/utane/GAUCHOPODER/
+# readonly DESTINO=/run/media/utane/AleAgus/
+readonly DESTINO=/run/media/utane/5043-93A3
+
+    if [[ ! -d "$DESTINO/Descargas" || ! -d "$DESTINO/Documentos" || ! -d "$DESTINO/Imagenes" || ! -d "$DESTINO/Musica" || ! -d "$DESTINO/Escritorio" ]]; then
+        mkdir -p "$DESTINO/Descargas" "$DESTINO/Documentos" "$DESTINO/Imagenes" "$DESTINO/Musica" "$DESTINO/Escritorio"
+    fi
 
 backup_folders() {
     local DIR TARGET
@@ -18,8 +23,11 @@ backup_folders() {
 }
 
 backup_files() {
-    find $HOME/Descargas -maxdepth 1 -mindepth 1 -type f -print -exec rsync -vz {} $DESTINO/Descargas \;
-    find $HOME/Documentos -maxdepth 1 -mindepth 1 -type f -print -exec rsync -vz {} $DESTINO/Documentos \;
+    find $HOME/Descargas -print -exec rsync -Pavz --exclude 'Series' {} "$DESTINO/Descargas" \;
+    find $HOME/Documentos -print -exec rsync -Pavz --exclude={'Libros','Mis Cosas'} {} "$DESTINO/Documentos" \;
+    rsync -Pavz --ignore-existing "$HOME/Imagenes" "$DESTINO/Imagenes"
+    rsync -Pavz --exclude={'Emulation','ISO','Images'} "$HOME/Escritorio" "$DESTINO/Escritorio"
+    rsync -Pavz --ignore-existing --exclude '*.flac' "$HOME/Musica" "$DESTINO/Musica"
     notify-send "Los archivos se terminaron de mandar a $DESTINO"
 }
 
@@ -28,7 +36,42 @@ main() {
     backup_files
 }
 
-main
+backup_files
+
+# This is still in testing
+# yet to be implemented
+# backup_files() {
+#     local DESCARGAS_TARGET="$DESTINO/Descargas"
+#     local DOCUMENTOS_TARGET="$DESTINO/Documentos"
+#     local NOW=$(date +%s)
+#     local THREE_DAYS_AGO=$((NOW - (3 * 24 * 3600)))  # 3 days ago in seconds since epoch
+#
+#     # Backup files in Descargas directory
+#     find "$HOME/Descargas" -maxdepth 1 -mindepth 1 -type f -print | while read file; do
+#         local filename=$(basename "$file")
+#         local dest="$DESCARGAS_TARGET/$filename"
+#         local mtime=$(stat -c %Y "$dest" 2>/dev/null || echo 0)
+#         if [[ $mtime -lt $THREE_DAYS_AGO ]]; then
+#             rsync -vz "$file" "$DESCARGAS_TARGET"
+#         else
+#             rsync -vz --ignore-existing "$file" "$DESCARGAS_TARGET"
+#         fi
+#     done
+#
+#     # Backup files in Documentos directory
+#     find "$HOME/Documentos" -maxdepth 1 -mindepth 1 -type f -print | while read file; do
+#         local filename=$(basename "$file")
+#         local dest="$DOCUMENTOS_TARGET/$filename"
+#         local mtime=$(stat -c %Y "$dest" 2>/dev/null || echo 0)
+#         if [[ $mtime -lt $THREE_DAYS_AGO ]]; then
+#             rsync -vz "$file" "$DOCUMENTOS_TARGET"
+#         else
+#             rsync -vz --ignore-existing "$file" "$DOCUMENTOS_TARGET"
+#         fi
+#     done
+#
+#     notify-send "Los archivos se terminaron de mandar a $DESTINO"
+# }
 
 # This is here for demonstrative purposes only
 # If we aren't transferring a DIRectory
